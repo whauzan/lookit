e9fb82eb2eba214d9f238a34219958f1b9f6b3c6
whauzan <wahyu172845@gmail.com>
2023-06-16 04:15:24 +0700

    navbar & oauth

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,45 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { buttonVariants } from './ui/Button';
+import { getAuthSession } from '@/lib/auth';
+
+const Navbar = async () => {
+  const session = await getAuthSession();
+  return (
+    <div className="fixed inset-x-0 top-0 z-10 h-fit border-b border-zinc-300 bg-zinc-100 py-2">
+      <div className="container mx-auto flex h-full max-w-7xl items-center justify-between gap-2">
+        {/* Logo */}
+        <Link href={'/'} className="flex items-center gap-2">
+          <div className="block w-12 sm:w-10 md:hidden">
+            <Image
+              src={'/logo.png'}
+              width={100}
+              height={100}
+              alt="Lookit Logo"
+            />
+          </div>
+          <div className="hidden w-24 sm:w-20 md:block">
+            <Image
+              src={'/logo-text.png'}
+              width={100}
+              height={100}
+              alt="Lookit Logo"
+            />
+          </div>
+        </Link>
+
+        {/* Search Bar */}
+
+        {session ? (
+          <p>{session.user.name}</p>
+        ) : (
+          <Link className={buttonVariants()} href={'/sign-in'}>
+            Sign In
+          </Link>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;

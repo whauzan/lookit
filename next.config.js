2a671ed852d39f4aee5aa964037e62ba555cebab
Wahyu Hauzan Rafi <whauzan@pro.local>
2025-10-26 08:17:12 +0700

    build: upgrade uploadthing version

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['lh3.googleusercontent.com', 'uploadthing.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'uploadthing.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+      },
+    ],
   },
   experimental: {
     appDir: true,
--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -5,11 +5,9 @@ const f = createUploadthing();
 
 export const ourFileRouter = {
   imageUploader: f({ image: { maxFileSize: '4MB' } })
-    .middleware(async (req) => {
+    .middleware(async ({ req }) => {
       const user = await getToken({ req });
-
       if (!user) throw new Error('Unauthorized');
-
       return { userId: user.id };
     })
     .onUploadComplete(async ({ metadata, file }) => {}),
